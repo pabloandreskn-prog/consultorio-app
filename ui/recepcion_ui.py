220f70fa040c0cb2bab02dda297d9537c2ddd854
pabloandreskn-prog <pabloandres.kn@gmail.com>
2026-01-30 02:07:48 -0300

    Create recepcion_ui.py

---
+++ ui/recepcion_ui.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def recepcion_ui():
+    st.header("🛎️ Modo Recepción")
+
+    st.info("Vista simplificada para agenda y cobros rápidos")
+
+    st.write("👉 Acá luego veremos:")
+    st.write("- Agenda del día")
+    st.write("- Marcar asistencia")
+    st.write("- Cobros rápidos")
