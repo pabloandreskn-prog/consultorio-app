d5d3e69c3515835d9ddd6f114ddff72d9a9c3cbd
pabloandreskn-prog <pabloandres.kn@gmail.com>
2026-01-30 01:45:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import streamlit as st
-from ui.pagos_ui import pagos_ui
-from ui.dashboard_ui import dashboard_ui
-
-
-from ui.pacientes_ui import pacientes_ui
-from ui.servicios_ui import servicios_ui
 from ui.agenda_ui import agenda_ui
-
 st.set_page_config(
     page_title="Consultorio",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
+st.markdown("""
+<style>
+:root {
+    --primary-color: #60b067;
+}
+</style>
+""", unsafe_allow_html=True)
 
 st.sidebar.title("Menú")
 
@@ -32,4 +32,5 @@ elif menu == "Cobros":
     pagos_ui()
 
 elif menu == "dashboard":
+    dashboard_ui()
     dashboard_ui()
