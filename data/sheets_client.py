47503dc04347f8ec16555ac8f7516f4466fe2b56
Pablo Alvarez <pabloandres.kn@gmail.com>
2026-01-30 00:30:11 -0300

    Usar secrets de Streamlit para Google Sheets

--- data/sheets_client.py
+++ data/sheets_client.py
@@ -1,19 +1,11 @@
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
 def get_client():
-    creds = Credentials.from_service_account_file(
-        "credentials.json",
-        scopes=SCOPES
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
     )
     return gspread.authorize(creds)
-
-def get_sheet(sheet_name):
-    client = get_client()
-    return client.open(sheet_name)
 
